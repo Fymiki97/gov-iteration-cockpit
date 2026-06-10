1375676300a2bd1c39de48bfd4bb59f8ba93777a
CloudSync <cloudsync@wpscomate.local>
2026-06-10 19:12:48 +0800

    重新发布

--- server/nitro.config.ts
+++ server/nitro.config.ts
@@ -10,8 +10,8 @@ export default defineNitroConfig({
     projectId: '',
     appBaseEndpoint: 'https://o.wpsgo.com/app/app-base',
     SESSION_SECRET: '7sK9dF2gP5aR8jL3zQ1bN6cV4mH0tG7wE',
-    WPS_APP_ID: '',
-    WPS_APP_SECRET: '',
+    WPS_APP_ID: 'AK20260401JJOYXC',
+    WPS_APP_SECRET: 'f59d077c03b8d65fa9f257c5ae3155e9',
     nitro: {
       envPrefix: '',
     },
